a4903ccb8a1fd4f36ae8a3320145c3caf9fcae1f
John Mannully <johnmannul@gmail.com>
2025-04-18 01:22:07 -0400

    mongodb.ts namespace fix

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,21 +3,12 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 const uri = process.env.MONGODB_URI;
 if (!uri) throw new Error("Please define the MONGODB_URI environment variable");
 
-// extend global object with custom type
-declare global {
-  //interface to declare the type
-  namespace NodeJS {
-    interface Global {
-      _mongoClientPromise?: Promise<MongoClient>;
-    }
-  }
+// augment global NodeJS.Global interface (ES2015)
+declare module globalThis {
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-const globalForMongo = global as typeof globalThis & {
-  _mongoClientPromise?: Promise<MongoClient>;
-};
-
-if (!globalForMongo._mongoClientPromise) {
+if (!globalThis._mongoClientPromise) {
   const client = new MongoClient(uri, {
     serverApi: {
       version: ServerApiVersion.v1,
@@ -25,9 +16,9 @@ if (!globalForMongo._mongoClientPromise) {
       deprecationErrors: true,
     },
   });
-  globalForMongo._mongoClientPromise = client.connect();
+  globalThis._mongoClientPromise = client.connect();
 }
 
-const clientPromise = globalForMongo._mongoClientPromise!;
+const clientPromise = globalThis._mongoClientPromise!;
 
 export default clientPromise;
