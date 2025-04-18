5ad8fbc5eed413de6f787a99ea6a4bb7f3091aec
John Mannully <johnmannul@gmail.com>
2025-04-18 01:25:20 -0400

    mongodb.ts shenanigans

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,22 +3,21 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 const uri = process.env.MONGODB_URI;
 if (!uri) throw new Error("Please define the MONGODB_URI environment variable");
 
-// augment global NodeJS.Global interface (ES2015)
-declare module globalThis {
+// augment global type
+declare global {
+  // eslint-disable-next-line no-var
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-if (!globalThis._mongoClientPromise) {
-  const client = new MongoClient(uri, {
+// use cached promise/make new one
+const clientPromise =
+  global._mongoClientPromise ??
+  (global._mongoClientPromise = new MongoClient(uri, {
     serverApi: {
       version: ServerApiVersion.v1,
       strict: true,
       deprecationErrors: true,
     },
-  });
-  globalThis._mongoClientPromise = client.connect();
-}
-
-const clientPromise = globalThis._mongoClientPromise!;
+  }).connect());
 
 export default clientPromise;
