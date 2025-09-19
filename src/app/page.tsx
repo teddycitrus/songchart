75b3b24d0c0234b757b5977ce9ca02837d9b5375
J M <69435473+teddycitrus@users.noreply.github.com>
2025-09-19 17:22:42 -0400

    add page reload after update is performed on a song entry

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -132,7 +132,7 @@ export default function Home() {
                 console.log("Unknown error", err);
               }
           } finally {
-            //window.location.reload(); //refreshes page
+            window.location.reload(); //refreshes page
           }
         }
     }
