73de035c91c0926b11ce31f3c75f17197480e662
John Mannully <johnmannul@gmail.com>
2025-09-23 16:45:36 -0400

    handled build errors idk

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Image from 'next/image';
-import { Song } from "../../types/Song";
+import { Song } from "../types/Song";
 
 export default function Home() {
     /* type Song = {
